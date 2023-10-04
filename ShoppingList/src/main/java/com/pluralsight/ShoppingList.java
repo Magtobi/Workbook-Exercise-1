322793442c80a88859ceb8ab6c49d305b63e43d4
Student <darrienconnor97@yahoo.com>
2023-10-04 11:47:55 -0400

    first commit

---
+++ ShoppingList/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,16 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("Milks");
+        System.out.println("Eggs");
+        System.out.println("Bread");
+        System.out.println("Watermelon");
+        System.out.println("Water");
+        System.out.println("Carrots");
+        System.out.println("Apples");
+        System.out.println("Flour");
+        System.out.println("Paper");
+        System.out.println("Chips");
+    }
+}
